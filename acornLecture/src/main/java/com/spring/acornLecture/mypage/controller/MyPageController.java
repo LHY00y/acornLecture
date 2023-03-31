baf295a13b6b3a50a21c3c7d562be6b42c61fd0a
gunhee93 <123151812+gunhee93@users.noreply.github.com>
2023-03-31 11:59:35 +0900

    mypage

--- acornLecture/src/main/java/com/spring/acornLecture/mypage/controller/MyPageController.java
+++ acornLecture/src/main/java/com/spring/acornLecture/mypage/controller/MyPageController.java
@@ -1,9 +1,12 @@
 package com.spring.acornLecture.mypage.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,20 +15,33 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.spring.acornLecture.lecture.dto.LectureDTO;
 import com.spring.acornLecture.member.dto.MemberDTO;
+import com.spring.acornLecture.mypage.service.MyPageService;
 
 @Controller
 @SessionAttributes("member")
 public class MyPageController {
 
+	@Autowired
+	private MyPageService myPageService;
 	
 	@RequestMapping(value="/member/mypage", method=RequestMethod.POST)
-	public ModelAndView login(@ModelAttribute("member") MemberDTO member, HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
+	public ModelAndView login(@ModelAttribute("member") MemberDTO member, RedirectAttributes rAttr,
+			HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
 		ModelAndView mav = new ModelAndView();
 		
+		HttpSession session = request.getSession();
+		Boolean isLogOn = (Boolean) session.getAttribute("isLogon");
 		
+		if(isLogOn == true) {
+			List<LectureDTO> myLectureList = myPageService.myLecture(member);
+			mav.addObject("myLectureList", myLectureList);
+			mav.setViewName("/mypageFrom");
+		} else {
+			mav.setViewName("redirect:/login.do");
+		}
 		
 		return mav;
 	}
--- acornLecture/src/main/java/com/spring/acornLecture/mypage/dto/MyPageDTO.java
+++
@@ -1,8 +0,0 @@
-package com.spring.acornLecture.mypage.dto;
-
-import java.util.List;
-
-public class MyPageDTO {
-
-	private List<String> lectureList;
-}
